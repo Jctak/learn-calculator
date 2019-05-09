580d7ac499df6d5e5a7336f3c34f52007d234af8
Jeong Jin Tak <jeongjintak7@gmail.com>
2019-05-09 10:41:41 -0400

    Added a conditional for the numbers to check if a result has been displayed or not.

--- js/script.js
+++ js/script.js
@@ -11,62 +11,101 @@ let hasOperator = false;
 let numQue, numQueTwo, total, operatorValue;
 
 // adding click handlers to number buttons
-numbers.forEach(function(number){
-  number.addEventListener('click', function(){
-    if(hasOperator === false){
+//-------------------------------------------------------
+// numbers.forEach(function(number){
+//   number.addEventListener('click', function(){
+//     if(hasOperator === false){
+//       input.innerHTML += this.innerHTML;
+//       numQue = parseInt(input.innerHTML, 10);
+//     }
+//     else if(hasOperator === true){
+//       input.innerHTML += this.innerHTML;
+//       numQueTwo = parseInt(input.innerHTML, 10);
+//     }
+//   });
+// });
+//-------------------------------------------------------
+
+Array.from(numbers).map(number => {
+  number.addEventListener("click", function(){
+    if (resultDisplayed === false){
       input.innerHTML += this.innerHTML;
-      numQue = parseInt(input.innerHTML, 10);
     }
-    else if(hasOperator === true){
+    else if(resultDisplayed === True && lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/"){
+      resultDisplayed = false;
+      input.innerHTML = this.innerHTML;
+    }
+    else{
+      resultDisplayed = false;
+      input.innerHTML = "";
       input.innerHTML += this.innerHTML;
-      numQueTwo = parseInt(input.innerHTML, 10);
     }
   });
 });
 
 // adding click handlers to the operation buttons
-operators.forEach(function(operator){
-  operator.addEventListener('click', function(){
-    hasOperator = true;
+//------------------------------------------------------
+// operators.forEach(function(operator){
+//   operator.addEventListener('click', function(){
+//     hasOperator = true;
+//
+//     if(numQue >= 0){
+//       input.innerHTML = this.innerHTML;
+//       operatorValue = input.innerHTML;
+//       if(operatorValue === '+'){
+//         operatorValue = 1;
+//       }else if (operatorValue === '-') {
+//         operatorValue = 2;
+//       }
+//       else if (operatorValue === '*') {
+//         operatorValue = 3;
+//       }
+//       else if (operatorValue === '/') {
+//         operatorValue = 4;
+//       }
+//     }
+//   });
+// });
 
-    if(numQue >= 0){
-      input.innerHTML = this.innerHTML;
-      operatorValue = input.innerHTML;
+//------------------------------------------------------
 
-      if(operatorValue === '+'){
-        operatorValue = 1;
-      }else if (operatorValue === '-') {
-        operatorValue = 2;
-      }
-      else if (operatorValue === '*') {
-        operatorValue = 3;
-      }
-      else if (operatorValue === '/') {
-        operatorValue = 4;
-      }
+Array.from(operators).map(operator => {
+  operator.addEventListener("click", function(e) {
+    let currentString = input.innerHTML;
+    let lastChar = currentString[currentString.length - 1];
+
+    if(lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/"){
+      const newString = currentString.substring(0, currentString.length - 1) + this.innerHTML;
+      input.innerHTML = newString;
+    }
+    else if(currentString == 0){
+      console.log("Enter a number first");
+    }
+    else{
+      input.innerHTML += e.target.innerHTML;
     }
   });
-});
+})
 
 // on click of 'equal' button
-result.addEventListener('click', function(){
-  if(operatorValue === 1){
-    total = (numQue + numQueTwo);
-    input.innerHTML = total;
-  }
-  else if(operatorValue === 2){
-    total = numQue + numQueTwo;
-    input.innerHTML = total;
-  }
-  else if(operatorValue === 3){
-    total = (numQue * numQueTwo);
-    input.innerHTML = total;
-  }
-  else if(operatorValue === 4){
-    total = (numQue / numQueTwo);
-    input.innerHTML = total;
-  }
-});
+// result.addEventListener('click', function(){
+//   if(operatorValue === 1){
+//     total = (numQue + numQueTwo);
+//     input.innerHTML = total;
+//   }
+//   else if(operatorValue === 2){
+//     total = numQue + numQueTwo;
+//     input.innerHTML = total;
+//   }
+//   else if(operatorValue === 3){
+//     total = (numQue * numQueTwo);
+//     input.innerHTML = total;
+//   }
+//   else if(operatorValue === 4){
+//     total = (numQue / numQueTwo);
+//     input.innerHTML = total;
+//   }
+// });
 
 // clearing the input on press of clear
 clear.addEventListener('click', function(){
